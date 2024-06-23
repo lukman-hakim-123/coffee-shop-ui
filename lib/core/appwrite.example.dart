8730b67313df29389ac9eee39b7b1057d0c09a5b
lh146244 <lh146244@gmail.com>
2024-06-23 02:04:35 +0000

    add core appwrite

---
+++ lib/core/appwrite.example.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String endpoint = 'https://cloud.appwrite.io/v1'; // Example endpoint
+  static const String projectId = 'YOUR_PROJECT_ID'; // Replace with your project ID
+  static const String databaseId = 'YOUR_DATABASE_ID'; // Replace with your database ID
+  static const String collectionId = 'YOUR_COLLECTION_ID'; // Replace with your collection ID
+}
