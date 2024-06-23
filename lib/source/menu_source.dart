b9630eef38f3d5d57a51656e3a2fcb59126550a7
lh146244 <lh146244@gmail.com>
2024-06-23 02:08:58 +0000

    add menu source

---
+++ lib/source/menu_source.dart
@@ -0,0 +1,26 @@
+import 'package:appwrite/models.dart';
+import 'package:coffee_shop/core/appwrite.dart';
+import '../models/menu.dart';
+import 'package:appwrite/appwrite.dart';
+
+class MenuSource {
+  static Client client = Client()
+      .setEndpoint(AppwriteConstants.endpoint)
+      .setProject(AppwriteConstants.projectId);
+  static Databases databases = Databases(client);
+
+  static Future<List<Menu>> getMenus() async {
+    try {
+      final response = await databases.listDocuments(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.collectionId,
+      );
+
+      return response.documents.map((Document doc) {
+        return Menu.fromMap(doc.data);
+      }).toList();
+    } catch (e) {
+      return [];
+    }
+  }
+}
