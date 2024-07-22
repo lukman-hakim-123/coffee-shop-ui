99ba103567bc4e1764def52db4124eea817318f3
lukman-hakim-123 <lh146244@gmail.com>
2024-07-22 20:47:24 +0700

    include appwrite.dart

---
+++ lib/core/appwrite.dart
@@ -0,0 +1,7 @@
+class AppwriteConstants {
+  static const String endpoint = 'https://cloud.appwrite.io/v1';
+  static const String projectId = '6676b9e80004b886136a';
+  static const String databaseId = '6676c287001a26203cb1';
+  static const String collectionId = '6676c28e003958f50a75';
+  static const String bucketId = '6676bc4f002ef3236fe1';
+}
